603bb4ee22db00880362d5f5aa164ac35520c031
Sam Barratt <milktheelephant@gmail.com>
2016-10-22 21:52:15 +0100

    removed image

--- whereabouts-pebble/src/c/whereabouts.c
+++ whereabouts-pebble/src/c/whereabouts.c
@@ -19,7 +19,7 @@ static void main_window_load(Window * window)
  //new text layer
   s_time_layer = text_layer_create(GRect (15, PBL_IF_ROUND_ELSE(64,128), bounds.size.w, 50));
   s_date_layer = text_layer_create(GRect (105, PBL_IF_ROUND_ELSE(64,128), bounds.size.w, 50));
-  s_background_bitmap =  gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+//  s_background_bitmap =  gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
 
 
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -28,10 +28,10 @@ static void main_window_load(Window * window)
   text_layer_set_text(s_date_layer,"date");
 
 
-  s_background_layer = bitmap_layer_create(bounds);
+//  s_background_layer = bitmap_layer_create(bounds);
 
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+ // bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  //layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
 
   //add the text as a child layer to the root window
@@ -63,8 +63,8 @@ static void tick_handler (struct tm * tick_time, TimeUnits units_changed)
 static void main_window_unload(Window * window)
 {
   text_layer_destroy(s_time_layer);  
-  gbitmap_destroy(s_background_bitmap);
-  bitmap_layer_destroy(s_background_layer);
+  //gbitmap_destroy(s_background_bitmap);
+  //bitmap_layer_destroy(s_background_layer);
   window_destroy(s_main_window);
   
 }
@@ -78,7 +78,7 @@ static void init()
                              (WindowHandlers) 
                              { .load = main_window_load, .unload = main_window_unload});
  
- window_set_background_color(s_main_window, GColorBlack);
+ //window_set_background_color(s_main_window, GColorBlack);
  window_stack_push(s_main_window, true);
 
  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
