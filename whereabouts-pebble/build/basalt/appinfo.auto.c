939128ac921293629e8c11cac380b47eadc83747
Sam Barratt <samathy@sbarratt.co.uk>
2016-10-22 17:56:00 +0100

    Initial commit of watch face - Displays time in correct place and placeholder background image

---
+++ whereabouts-pebble/build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "whereabouts",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_BASALT,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x84, 0x79, 0x6D, 0x5D, 0x2B, 0x21, 0x40, 0x34, 0xBD, 0x29, 0x4A, 0x06, 0xB4, 0x3A, 0x91, 0xFC },
+  .virtual_size = 0xb6b6
+};
